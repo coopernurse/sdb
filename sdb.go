094134dfdf727779e2a7f70d535fda05c3aa2145
Roland Bali <roland.bali@spagettikod.se>
2014-10-31 07:53:53 +0100

    Added licensing

--- sdb.go
+++ sdb.go
@@ -1,3 +1,31 @@
+// Copyright (c) 2014, Roland Bali (roland.bali@spagettikod.se), Spagettikod
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this
+//    list of conditions and the following disclaimer in the documentation and/or
+//    other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may
+//    be used to endorse or promote products derived from this software without
+//    specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 package sdb
 
 import (
@@ -169,7 +197,6 @@ func (sdb *SimpleDB) post(v interface{}) (err error) {
 	return
 }
 
-// Attributes
 func NewAttribute(name string, value string) *Attribute {
 	a := &Attribute{Name: name, Value: value, Replace: false}
 	return a
@@ -208,8 +235,6 @@ func NewSimpleDB(a string, s string, r string) *SimpleDB {
 
 	return sdb
 }
-
-// Commands
 
 func (sdb *SimpleDB) ListDomains() (r ListDomainsResponse, err error) {
 	sdb.resetParameters()
